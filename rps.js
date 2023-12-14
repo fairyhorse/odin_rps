eb58f2a2a8c9445bb004e4e15f48bf911512bb77
Dana Muktubayeva <dmuktubayeva@gmail.com>
2023-12-14 12:35:57 +0600

    index.html

--- rps.js
+++ rps.js
@@ -1,30 +1,53 @@
 const form = document.querySelector('#form');
 const input = document.querySelector('#input');
-// const computer = document.querySelector('#computer');
+const player = document.querySelector('#player')
+const computer = document.querySelector('#computer');
+const output = document.querySelector('#output');
 
-// const moves = ['rock', 'paper', 'scissors'];
+const moves = [ 'paper', 'rock', 'scissors'];
 
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    function onlyLettersAndNumbers(str) {
-        return Boolean(str.match(/^[A-Za-z0-9]*$/));
-    }
+    const item = input.value.toLowerCase();
 
-    if (!onlyLettersAndNumbers(item) || item === '') {
+    let onlyLettersAndNumbers = (str) => Boolean(str.match(/^[a-zA-Z]+$/));
+
+    if (!moves.includes(item) || !onlyLettersAndNumbers(item) || item === '') {
         input.value = '';
         alert('ENTER A VALID ITEM NAME');
     } else {
+        player.innerText = item;
         input.value = '';
     }
 })
 
 
-// function round(playerSelection, computerSelection) {
-    
-// }
+function playRound(playerSelection, computerSelection) {
+    computer.innerText = computerSelection;
+    // paper -> rock -> scissors -> paper
+    // [ paper  rock  scissors]
+    //     0     1       2
 
-// console.log(round(pl_sl, () => moves[Math.floor(Math.random() * 3)]));
+    // player:     1 rock          2 scissors       0 paper          2 scissors      1 rock        0 paper 
+    // computer:   2 scissors      1 rock           2 scissors       0 paper         0 paper       1 rock
+    //             -1              1                -2               2               1             -1
+    //             win             lose             lose             win             lose          win
+    // win:  -1,  2
+    // lose:  1, -2
+
+    let diff = moves.indexOf(playerSelection) - moves.indexOf(computerSelection);
+
+    if (playerSelection == computerSelection){
+        output.innerText = "It's a tie!"
+    } else if (diff == -1 || diff == 2){
+        output.innerText = "You win!"
+    } else {
+        output.innerText = "You lose!"
+    }
+}
+
+playRound(player.innerText, () => moves[Math.floor(Math.random() * 3)]);
 
 
