cffdce530486391fd0788413c3c1ff3fb18879e3
Dana Muktubayeva <dmuktubayeva@gmail.com>
2023-12-14 13:59:57 +0600

    rps.js

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
-const form = document.querySelector('#form');
-const input = document.querySelector('#input');
+const form = document.querySelector('#form-move');
+const input = document.querySelector('#input-move');
 const player = document.querySelector('#player')
 const computer = document.querySelector('#computer');
 const output = document.querySelector('#output');
@@ -7,47 +7,72 @@ const output = document.querySelector('#output');
 const moves = [ 'paper', 'rock', 'scissors'];
 
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+function game(rounds) {
 
-    const item = input.value.toLowerCase();
-
-    let onlyLettersAndNumbers = (str) => Boolean(str.match(/^[a-zA-Z]+$/));
-
-    if (!moves.includes(item) || !onlyLettersAndNumbers(item) || item === '') {
-        input.value = '';
-        alert('ENTER A VALID ITEM NAME');
-    } else {
-        player.innerText = item;
-        input.value = '';
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+    
+        const item = input.value.toLowerCase();
+    
+        let onlyLettersAndNumbers = (str) => Boolean(str.match(/^[a-zA-Z]+$/));
+    
+        if (!moves.includes(item) || !onlyLettersAndNumbers(item) || item === '') {
+            input.value = '';
+            alert('ENTER A VALID ITEM NAME');
+        } else {
+            player.innerText = item;
+            input.value = '';
+            
+            let getComputerSelection = () => moves[Math.floor(Math.random() * 3)];
+            playRound(player.innerText, getComputerSelection());
+        }
+    })
+    
+    
+    function playRound(playerSelection, computerSelection) {
+        computer.innerText = computerSelection;
+    
+    
+        // paper -> rock -> scissors -> paper
+        // [ paper  rock  scissors]
+        //     0     1       2
+    
+        // ======================================================================================================
+        // player:     1 rock          2 scissors       0 paper          2 scissors      1 rock        0 paper 
+        // computer:   2 scissors      1 rock           2 scissors       0 paper         0 paper       1 rock
+        //             -1              1                -2               2               1             -1
+        //             win             lose             lose             win             lose          win
+        // ------------------------------------------------------------------------------------------------------
+        // win:  -1,  2
+        // lose:  1, -2
+        // ======================================================================================================
+    
+        let diff = moves.indexOf(playerSelection) - moves.indexOf(computerSelection);
+        
+        let playerScore;
+        let computerScore;
+    
+        if (playerSelection == computerSelection){
+            output.innerText = "It's a tie!";
+        } else if (diff == -1 || diff == 2){
+            output.innerText = "You win!";
+            playerScore++;
+    
+        } else {
+            output.innerText = "You lose!";
+            computerScore++;
+        }
     }
-})
 
 
-function playRound(playerSelection, computerSelection) {
-    computer.innerText = computerSelection;
-    // paper -> rock -> scissors -> paper
-    // [ paper  rock  scissors]
-    //     0     1       2
-
-    // player:     1 rock          2 scissors       0 paper          2 scissors      1 rock        0 paper 
-    // computer:   2 scissors      1 rock           2 scissors       0 paper         0 paper       1 rock
-    //             -1              1                -2               2               1             -1
-    //             win             lose             lose             win             lose          win
-    // win:  -1,  2
-    // lose:  1, -2
-
-    let diff = moves.indexOf(playerSelection) - moves.indexOf(computerSelection);
-
-    if (playerSelection == computerSelection){
-        output.innerText = "It's a tie!"
-    } else if (diff == -1 || diff == 2){
-        output.innerText = "You win!"
-    } else {
-        output.innerText = "You lose!"
+    while (playerScore < rounds && computerScore < rounds){
+        playRound();        
     }
+    if (playerScore > computerScore){
+            output.innerText = "YOU WON!";
+        } else {
+            output.innerText = "YOU LOST!"
+        }
 }
 
-playRound(player.innerText, () => moves[Math.floor(Math.random() * 3)]);
-
-
+game(5);
